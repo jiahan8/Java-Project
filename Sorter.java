7755cd24cd7a4944a8bfb7682eca46878212f3b0
JiaHan <jiahantan96@gmail.com>
2017-02-10 16:56:55 -0600

    Create Sorter.java

---
+++ Sorter.java
@@ -0,0 +1,16 @@
+package edu.iastate.cs228.hw2;
+/*
+ * @ Jia Han Tan
+ */
+import java.util.Comparator;
+
+/* Nothing to be done here. */
+
+public interface Sorter {
+	/**
+	 * Sorts words according to the Comparator comp.
+	 * @param words words to be sorted.
+	 * @param comp Comparator used to sort words.
+	 */
+	public void sort(String[] words, Comparator<String> comp);
+}
